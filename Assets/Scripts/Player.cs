6f3618d835e1d2711b881a0dca75b30d50c5ca38
Ulysses <ullises@mail.ru>
2018-09-08 18:40:06 +0300

    Add joystick input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    #region private fields
+
+    private Rigidbody rb;
+
+    #endregion
+
+
+
+
+
+    #region serialize Fields
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    [SerializeField]
+    private SimpleTouchController leftStick;
+
+    [SerializeField]
+    private SimpleTouchController rightStick;
+
+    #endregion
+
+
+    #region Unity lifeCycle
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        PlayerControl();
+    }
+
+    #endregion
+
+
+    #region private methods
+
+    void PlayerControl()
+    {
+        rb.MovePosition(transform.position + Vector3.right * speed * leftStick.GetTouchPosition.x + Vector3.forward * speed * leftStick.GetTouchPosition.y);
+
+
+        Vector3 tmpAngles = transform.localEulerAngles;
+         tmpAngles.y = Vector3.Angle(new Vector3(0, 1), rightStick.GetTouchPosition);
+
+        if (rightStick.GetTouchPosition.x < 0)
+            tmpAngles.y = 360 - tmpAngles.y;
+        transform.localEulerAngles = tmpAngles;
+
+    }
+
+
+
+
+
+    #endregion
+
+}
+

2f3b925721a17e26f076fa26a556b1bc66f20778
Ulysses <ullises@mail.ru>
2018-09-10 10:30:08 +0300

    Add slow Zombie

---
+++ Assets/Scripts/SlowZombie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowZombie : Zombie {
+
+
+    #region Unity lifecycle
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "bullet")
+            Debug.Log("Shot him!");
+
+    }
+
+
+    #endregion
+
+
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Zombie : MonoBehaviour {
+
+    #region Serializable fields
+    [SerializeField]
+    protected float speed;
+
+    [SerializeField]
+    protected float maxHP;
+
+    [SerializeField]
+    protected float damagePerHit;
+
+    #endregion
+
+}
